5a2e4b2758664cc4e1438bcdeeff90b9fa99c932
Prapti Sanghavi <sanghavi.pr@northeastern.edu>
2025-10-23 23:04:40 -0400

    Create drug_interactions.py

---
+++ medical-qa-finetuning/src/medical/drug_interactions.py
@@ -0,0 +1,10 @@
+# Comprehensive drug interaction database
+        self.drug_interactions = {
+            'warfarin': ['aspirin', 'ibuprofen', 'nsaids', 'vitamin k', 'amiodarone'],
+            'metformin': ['contrast dye', 'alcohol', 'cimetidine', 'furosemide'],
+            'ssri': ['maoi', 'tramadol', 'linezolid', 'st johns wort'],
+            'ace_inhibitor': ['potassium', 'nsaids', 'lithium', 'aliskiren'],
+            'statins': ['grapefruit', 'gemfibrozil', 'niacin', 'cyclosporine'],
+            'beta_blockers': ['verapamil', 'diltiazem', 'clonidine'],
+            'digoxin': ['quinidine', 'verapamil', 'amiodarone', 'spironolactone']
+        }
