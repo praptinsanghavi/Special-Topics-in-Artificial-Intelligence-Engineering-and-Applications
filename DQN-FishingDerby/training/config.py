df9d024b0e88e9cb0c7e3c0e0ccb1c2383fb3c5a
Prapti Sanghavi <sanghavi.pr@northeastern.edu>
2025-11-07 21:53:20 -0500

    Update config.py

--- DQN-FishingDerby/training/config.py
+++ DQN-FishingDerby/training/config.py
@@ -1,1 +1,56 @@
-
+experiments_config = [
+        {
+            'episodes': 5000,  # TA requirement: minimum 5000
+            'learning_rate': 0.00025,
+            'gamma': 0.8,  # Assignment baseline
+            'epsilon_decay': 0.99,
+            'batch_size': 32,
+            'exploration_policy': 'epsilon_greedy',
+            'experiment_name': 'Baseline_gamma08'
+        },
+        {
+            'episodes': 5000,
+            'learning_rate': 0.00025,
+            'gamma': 0.99,  # Higher gamma test
+            'epsilon_decay': 0.99,
+            'batch_size': 32,
+            'exploration_policy': 'epsilon_greedy',
+            'experiment_name': 'HighGamma_099'
+        },
+        {
+            'episodes': 5000,
+            'learning_rate': 0.001,  # Higher learning rate
+            'gamma': 0.8,
+            'epsilon_decay': 0.99,
+            'batch_size': 32,
+            'exploration_policy': 'epsilon_greedy',
+            'experiment_name': 'HighLR_001'
+        },
+        {
+            'episodes': 5000,
+            'learning_rate': 0.00025,
+            'gamma': 0.8,
+            'epsilon_decay': 0.99,
+            'batch_size': 32,
+            'exploration_policy': 'boltzmann',
+            'experiment_name': 'Boltzmann'
+        },
+        {
+            'episodes': 5000,
+            'learning_rate': 0.00025,
+            'gamma': 0.8,
+            'epsilon_decay': 0.99,
+            'batch_size': 32,
+            'exploration_policy': 'ucb',
+            'experiment_name': 'UCB'
+        },
+        {
+            'episodes': 5000,
+            'learning_rate': 0.00025,
+            'gamma': 0.8,
+            'epsilon_decay': 0.995,  # Slower decay
+            'batch_size': 32,
+            'exploration_policy': 'epsilon_greedy',
+            'experiment_name': 'SlowDecay_0995'
+        }
+    ]
